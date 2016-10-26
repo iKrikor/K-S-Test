9f4503a846262c952de97797d480cda27595db5e
Krikor Bisdikian <iKrikor@krikors-macbook-pro.local>
2016-10-26 17:34:42 -0500

    Initial Commit

---
+++ K-S Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  K-S Test
+//
+//  Created by Krikor Bisdikian on 10/26/16.
+//  Copyright © 2016 Krikor Bisdikian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
